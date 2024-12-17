09aca8537f1ae0354cea32febadd963efa1d397a
naim-heera <abunaimheera@gmail.com>
2024-12-17 20:32:31 +0200

    Fixing capitalization

--- string.js
+++ string.js
@@ -54,22 +54,38 @@
 // // to change all occurrences of the substring
 // // console.log(d.replaceAll("i", "x"))
 // Filtering greeting messages
+// const list = document.querySelector(".output ul")
+// list.textContent = ""
+// const greetings = [
+//   "Happy Birthday!",
+//   "Merry Christmas my love",
+//   "A happy Christmas to all the family",
+//   "You're all I want for Christmas",
+//   "Get well soon",
+// ]
+
+// for (const greeting of greetings) {
+//   // Your conditional test needs to go inside the parentheses
+//   // in the line below, replacing what's currently there
+//   if (greeting.includes("Christmas")) {
+//     const listItem = document.createElement("li")
+//     listItem.textContent = greeting
+//     list.appendChild(listItem)
+//   }
+// }
+// Fixing capitalization
 const list = document.querySelector(".output ul")
 list.textContent = ""
-const greetings = [
-  "Happy Birthday!",
-  "Merry Christmas my love",
-  "A happy Christmas to all the family",
-  "You're all I want for Christmas",
-  "Get well soon",
-]
+const cities = ["lonDon", "ManCHESTer", "BiRmiNGHAM", "liVERpoOL"]
 
-for (const greeting of greetings) {
-  // Your conditional test needs to go inside the parentheses
-  // in the line below, replacing what's currently there
-  if (greeting.includes("Christmas")) {
-    const listItem = document.createElement("li")
-    listItem.textContent = greeting
-    list.appendChild(listItem)
-  }
+for (const city of cities) {
+  // write your code just below here
+  const lower = city.toLowerCase()
+  const firstletter = lower.slice(0, 1)
+  const capitalized = lower.replace(firstletter, firstletter.toUpperCase())
+
+  const result = capitalized
+  const listItem = document.createElement("li")
+  listItem.textContent = result
+  list.appendChild(listItem)
 }
