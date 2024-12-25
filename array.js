0fbb6b33d29ff5fa19cf146731fafe62719eead5
naim-heera <abunaimheera@gmail.com>
2024-12-25 20:34:36 +0200

    Until converting between strings and array done

--- array.js
+++ array.js
@@ -19,12 +19,39 @@
 // const index = cities.indexOf("Cardiff")
 
 // Note: In this call to splice(), the first argument says where to start removing items, and the second argument says how many items should be removed
-const cities = ["Manchester", "Liverpool", "Edinburgh", "Carlisle"]
-const index = cities.indexOf("Liverpool")
-console.log(index)
+// const cities = ["Manchester", "Liverpool", "Edinburgh", "Carlisle"]
+// const index = cities.indexOf("Liverpool")
+// console.log(index)
 
-console.log(index)
-if (index !== -1) {
-  cities.splice(index, 1)
-}
-console.log(cities)
+// console.log(index)
+// if (index !== -1) {
+//   cities.splice(index, 1)
+// }
+// console.log(cities)
+// Accessing every item
+// const birds = ["Parrot", "Falcon", "Owl"]
+// for (const bird of birds) {
+//   console.log(bird)
+// }
+// Doubling number with map
+// function double(number) {
+//   return number * 2
+// }
+// const numbers = [1, 2, 3, 4]
+// const doubled = numbers.map(double)
+// console.log(doubled)
+// Filter returns a new arrary of the function returns true
+// function isLong(city) {
+//   return city.length > 8
+// }
+// const cities = ["London", "Liverpool", "Totnes", "Edinburgh"]
+// const cityLength = cities.filter(isLong)
+// console.log(cityLength)
+// Converting between strings and arrays
+// const data = "Manchester,London,Liverpool,Birmingham,Leeds,Carlisle"
+// const cities = data.split(",")
+// console.log(cities)
+// const commaSeperated = cities.join(",")
+// console.log(commaSeperated)
+// const toString = cities.toString()
+// console.log(toString)
--- string.js
+++ string.js
@@ -90,29 +90,29 @@
 //   list.appendChild(listItem)
 // }
 // Making new strings from old parts
-const list = document.querySelector(".output ul")
-list.textContent = ""
-const stations = [
-  "MAN675847583748sjt567654;Manchester Piccadilly",
-  "GNF576746573fhdg4737dh4;Greenfield",
-  "LIV5hg65hd737456236dch46dg4;Liverpool Lime Street",
-  "SYB4f65hf75f736463;Stalybridge",
-  "HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield",
-]
+// const list = document.querySelector(".output ul")
+// list.textContent = ""
+// const stations = [
+//   "MAN675847583748sjt567654;Manchester Piccadilly",
+//   "GNF576746573fhdg4737dh4;Greenfield",
+//   "LIV5hg65hd737456236dch46dg4;Liverpool Lime Street",
+//   "SYB4f65hf75f736463;Stalybridge",
+//   "HUD5767ghtyfyr4536dh45dg45dg3;Huddersfield",
+// ]
 
-for (const station of stations) {
-  // write your code just below here
-  const code = station.slice(0, 3)
-  console.log(code)
-  const semiColon = station.indexOf(";")
-  console.log(semiColon)
-  const name = station.slice(semiColon + 1)
-  console.log(name)
+// for (const station of stations) {
+//   // write your code just below here
+//   const code = station.slice(0, 3)
+//   console.log(code)
+//   const semiColon = station.indexOf(";")
+//   console.log(semiColon)
+//   const name = station.slice(semiColon + 1)
+//   console.log(name)
 
-  const result = `${code}: ${name}`
-  console.log(result)
+//   const result = `${code}: ${name}`
+//   console.log(result)
 
-  const listItem = document.createElement("li")
-  listItem.textContent = result
-  list.appendChild(listItem)
-}
+//   const listItem = document.createElement("li")
+//   listItem.textContent = result
+//   list.appendChild(listItem)
+// }
